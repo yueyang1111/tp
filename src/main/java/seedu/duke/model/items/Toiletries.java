79a97ae8382a06efec22afdae75d746012b27b1c
LUKE\lukel <lukelousg@gmail.com>
2026-03-09 10:28:49 +0800

    Complete Snack and Toiletries subclasses

--- src/main/java/seedu/duke/model/items/Toiletries.java
+++ src/main/java/seedu/duke/model/items/Toiletries.java
@@ -3,4 +3,35 @@ package seedu.duke.model.items;
 import seedu.duke.model.Item;
 
 public class Toiletries extends Item {
+    private String brand;
+    private boolean isLiquid;
+
+    public Toiletries(String name, int quantity, String binLocation, String brand, boolean isLiquid) {
+        super(name, quantity, binLocation);
+        this.brand = brand;
+        this.isLiquid = isLiquid;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public boolean isLiquid() {
+        return isLiquid;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setLiquid(boolean liquid) {
+        isLiquid = liquid;
+    }
+
+    @Override
+    public String toString() {
+        return "[Toiletries] " + super.toString()
+                + ", Brand: " + brand
+                + ", Liquid: " + isLiquid;
+    }
 }
