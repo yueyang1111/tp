1e8c8fab08c0de1c4e144f4078827d4548110b73
LUKE\lukel <lukelousg@gmail.com>
2026-04-13 22:24:17 +0800

    Refactor Code

--- src/main/java/seedu/inventorydock/model/Item.java
+++ src/main/java/seedu/inventorydock/model/Item.java
@@ -1,34 +1,5 @@
 package seedu.inventorydock.model;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-/**
- * Represents an item stored in the inventory.
- * An <code>Item</code> object stores a name, quantity, bin location, and expiry date.
- */
-public class Item {
-    private static final Logger logger = Logger.getLogger(Item.class.getName());
-    private String name;
-    private int quantity;
-    private String binLocation;
-    private String expiryDate;
-
-    /**
-     * Creates an item with the specified details.
-     *
-     * @param name Name of the item.
-     * @param quantity Quantity of the item.
-     * @param binLocation Bin location of the item.
-     * @param expiryDate Expiry date of the item.
-     */
-    public Item(String name, int quantity, String binLocation,
-                String expiryDate) {
-        this.name = name;
-        this.quantity = quantity;
-        this.binLocation = binLocation;
-        this.expiryDate = expiryDate;
-    }
 
     public String getName() {
         return name;
@@ -59,15 +30,6 @@ public class Item {
         this.binLocation = binLocation;
     }
 
-    public void setExpiryDate(String expiryDate) {
-        logger.log(Level.INFO, "Updating expiry date for item '" + name + "' from '"
-                + this.expiryDate + "' to '" + expiryDate + "'.");
-        this.expiryDate = expiryDate;
-    }
-
-    public String getExpiryDate() {
-        return expiryDate;
-    }
 
     /**
      * Returns the storage format used when persisting the item.
@@ -85,9 +47,6 @@ public class Item {
 
     @Override
     public String toString() {
-        return String.format(
-                "Name: %s, Quantity: %d, Bin: %s, Expiry: %s",
-                name, quantity, binLocation, expiryDate
-        );
+        
     }
 }
